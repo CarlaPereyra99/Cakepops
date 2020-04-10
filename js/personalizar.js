da7e03aec8dd4b8aa7de7596fa8f4dbbc83cc683
Julio <juliolira129@gmail.com>
2020-04-09 20:34:50 -0500

    Comentarios
    Se agregaron los comentarios a la vista de Personalización

--- js/personalizar.js
+++ js/personalizar.js
@@ -1,27 +1,37 @@
+    //Se llama a los botones y se almacenan
 var boton4 = document.getElementById('b4'),
     boton6 = document.getElementById('b6'),
     boton10 = document.getElementById('b10'),
     boton12 = document.getElementById('b12'),
+    //Se llama a las imagenes-diagrama
     foto4 = document.getElementById('p4'),
     foto6 = document.getElementById('p6'),
     foto10 = document.getElementById('p10'),
     foto12 = document.getElementById('p12'),
+    //Se llama a los contenedores de las i-d y el precio
     cont = document.getElementById('cont'),
     precio = document.getElementById('precio'),
+    //variable de control del tamaño de la separación 
     ctrls = 'offset-l2',
     ctrlp = 'offset-l1';
 
+    //Método que al escuchar el clic del boton de 4 piezas,
+    //aparece la i-d de 4 piezas y se separa 
+    //de la tarjeta con los botones para centrarla
 boton4.addEventListener('click', function(e){
-    foto4.classList.remove('hide');
-    foto6.classList.add('hide');
+    foto4.classList.remove('hide'); //quita la clase 'hide' de la imagen para aparecerce
+    foto6.classList.add('hide');    //Agrega 'hide' para ocultar las demás
     foto10.classList.add('hide');
     foto12.classList.add('hide');
-    cont.classList.replace(ctrls,'offset-l2');
-    precio.classList.replace(ctrlp,'offset-l1');
-    ctrls = 'offset-l2';
+    cont.classList.replace(ctrls,'offset-l2'); //reemplaza el tamaño de la separación de la i-d de los botones
+    precio.classList.replace(ctrlp,'offset-l1'); //lo mismo pero para la tarjeta del costo de la i-d
+    ctrls = 'offset-l2';    //Se guarda el tamaño de las separaciones
     ctrlp = 'offset-l1';
 });
 
+    //Método que al escuchar el clic del boton de 6 piezas,
+    //aparece la i-d de 6 piezas y se separa 
+    //de la tarjeta con los botones para centrarla
 boton6.addEventListener('click', function(e){
     foto6.classList.remove('hide');
     foto4.classList.add('hide');
@@ -33,6 +43,9 @@ boton6.addEventListener('click', function(e){
     ctrlp = 'offset-l8';
 });
 
+    //Método que al escuchar el clic del boton de 10 piezas,
+    //aparece la i-d de 10 piezas y se separa 
+    //de la tarjeta con los botones para centrarla
 boton10.addEventListener('click', function(e){
     foto10.classList.remove('hide');
     foto6.classList.add('hide');
@@ -44,6 +57,9 @@ boton10.addEventListener('click', function(e){
     ctrlp = 'offset-l8';
 });
 
+    //Método que al escuchar el clic del boton de 12 piezas,
+    //aparece la i-d de 12 piezas y se separa 
+    //de la tarjeta con los botones para centrarla
 boton12.addEventListener('click', function(e){
     foto12.classList.remove('hide');
     foto6.classList.add('hide');
--- views/persPedido.php
+++ views/persPedido.php
@@ -21,6 +21,10 @@
     </header>
     <br>
     <div class="row">
+        <!-- Renglón que contiene los botones de selección de tamaño y el diagrama del tamaño -->
+        <!--Una tarjeta que cubre 2 columnas en pantalla grande y está a 1 columa de distancia del margen,
+            en pantalla mediana cubre 3 columnas y está a una columna del margen,
+            en pantalla chica cubre 8 columnas y está a 2 columnas del margen -->
         <div class="col s8 offset-s2 m3 offset-m1 l2 offset-l1 card" style="margin-top: 0px; padding: 0px">
             <div class="card-action" style="height: 50px; padding: 15px">
                 <h3 class="center-align" style="margin-top: 0px">Tamaño de Caja</h3>
@@ -36,7 +40,10 @@
                     style="margin-top: 10px; margin-bottom: 15px" id="b12">12 pz</a>
             </div>
         </div>
-
+        <!--Columna que contiene una tarjeta con las imagenes-diagrama del tamaño de las cajas 
+            previamente solicitadas en tamaños de 4, 6, 10 y 12 unidades, cada imagen esta condicionada
+            a aparecer solo cuando es presionado su botón correspondiente (vea /js/personalizar.js).
+            La tarjeta se adapta a la imagen-diagrama y se mueve conforme cambia el tamaño-->
         <div class="col offset-s1 offset-m1 offset-l2 card center-align" style="margin-top: 0px; padding: 0px"
             id="cont">
             <div class="container col s12 m12 l12" style="padding: 20px;">
@@ -47,8 +54,9 @@
             </div>
         </div>
 
+        <!--Pequeña tarjeta que muestra el precio a pagar-->
         <div class="card container col l2 offset-l1" style="margin-top: 0px" id="precio">
-            <h3 class="white-text">Costo: <input disabled value="" class="white-text center-align" type="number"
+            <h3 class="white-text">Costo: $<input disabled value="" class="white-text center-align" type="number"
                     name="precio" id="precio"></h3>
         </div>
 
